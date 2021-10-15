eb36df46c3613a19a782d1a75613aa02ecc9db4d
vagrant <vagrant@vagrant.vm>
2021-10-15 05:16:03 +0000

    before_delete_image_files

--- app/controllers/public/images_controller.rb
+++ app/controllers/public/images_controller.rb
@@ -12,70 +12,23 @@ class Public::ImagesController < ApplicationController
     	    fp.write  uploaded_file.read
         end
 
-
-        # api_key = "crafty-centaur-327921-7d5004602e5b.json" 
-        # api_url = URI("https://vision.googleapis.com/v1/images:annotate?key=#{api_key}")
-        # body=   {
-        #             "requests": [
-        #                 {
-        #                 "image": {
-        #                     "source": {
-        #                     "imageUri": "Rails.root.join('tmp/images', uploaded_file.original_filename)"
-        #                     }
-        #                 },
-        #                 "features": [
-        #                     {
-        #                     "type": "TEXT_DETECTION"
-        #                     }
-        #                 ]
-        #                 }
-        #             ]
-        #         }.to_json
-
-        # headers = { "Content-Type" => "application/json" }
-
-        # response = Net::HTTP.post(api_url, body, headers)
-        # byebug
-
-
-
-
-
-        # require "google/cloud/vision"
-
-        # image_annotator = Google::Cloud::Vision::ImageAnnotator.new
-
-        #google vision apiを用いてjson形式で投稿された画像のデータを取得。
-
-        #Bottleモデルの中でbottle_nameカラム(できればfeatureとageも参照)がjson形式で取得したデータと一致するものを全て取り出す。=>@bottlesに代入。
-        # Bottle.all.each do |bottle|
-            #もしもbottleとjsonのデータが一致すれば=>@bottlesに加える。
-        # end
-
-        #bottles/indexのビューファイルをrenderする。
-
-
-
-        require "google/cloud/vision" 
-
-        image_path = '/vagrant/whisky_for_you/tmp/images/S__9699343.jpg'
-
+        require "google/cloud/vision"
+        image_path = "#{output_path}"
         image_annotator = Google::Cloud::Vision.image_annotator
-
         response = image_annotator.text_detection(
         image:       image_path,
         max_results: 1 # optional, defaults to 10
         )
 
         response.responses.each do |res|
-        res.text_annotations.each do |text|
-            puts text.description
-            text.bounding_poly.vertices.each do | vertex |
-                byebug
-            puts "  x: #{vertex.x}, y: #{vertex.y}" 
+            res.text_annotations.each do |text|
+                puts text.description
+                text.bounding_poly.vertices.each do | vertex |
+                    byebug
+                puts "  x: #{vertex.x}, y: #{vertex.y}" 
+                end
             end
         end
-end
 
         #最後に写真を削除
         redirect_to image_path
